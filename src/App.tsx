10f77bf3217c85690b2253b0d4575a3a7e2d76bf
Luis Farid Chaires Pasalagua <luis.farid@outlook.com>
2023-09-28 14:23:49 -0600

    06

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,6 @@ function App() {
   const { data, cart, addToCart, removeFromCart, decreaseQuantity, increaseQuantity, clearCart, isEmpty, cartTotal } = useCart()
   const [state, dispatch] = useReducer(cartReducer, initialState)
 
-  console.log(state)
-
   return (
     <>
       <Header 
@@ -27,14 +25,13 @@ function App() {
           <h2 className="text-center">Nuestra Colección</h2>
 
           <div className="row mt-5">
-              {data.map((guitar) => (
+              {state.data.map((guitar) => (
                   <Guitar 
                     key={guitar.id}
                     guitar={guitar}
                     addToCart={addToCart}
                   />
               ))}
-              
           </div>
       </main>
 
