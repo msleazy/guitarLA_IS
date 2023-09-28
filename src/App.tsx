e8eed6c528678c6bc8e681e43d95e2996d19b041
Luis Farid Chaires Pasalagua <luis.farid@outlook.com>
2023-09-28 17:13:53 -0600

    09

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { cartReducer, initialState } from "./reducers/cart-reducer"
 
 function App() {
 
-  const { cart, addToCart, removeFromCart, decreaseQuantity, increaseQuantity, clearCart, isEmpty, cartTotal } = useCart()
+  const { cart, removeFromCart, decreaseQuantity, increaseQuantity, clearCart, isEmpty, cartTotal } = useCart()
   const [state, dispatch] = useReducer(cartReducer, initialState)
 
   return (
